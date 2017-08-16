3cde6c310572e5825baedd99c12d429b27cb7e58
Rustam Kasad <rustamkasad@gmail.com>
2017-08-16 14:31:20 -0700

    Add more jobs, listeners, etc.

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 Queue = require('bull');
 
-const jobQueue = new Queue('a job queue', 'redis://127.0.0.1:6379');
+const uno = new Queue('UNO queue', 'redis://127.0.0.1:6379');
+const dos = new Queue('DOS queue', 'redis://127.0.0.1:6379');
 
-jobQueue.process((job, done) => {
+uno.empty();
+dos.empty();
+
+uno.process((job, done) => {
   setTimeout(() => { 
-    console.log('JOB', job.data);
-    // console.log('job:', job.name);
+    console.log('Running...', { jobData: job.data, queue: job.queue.name});
     done();
-  }, 1000)
+  }, 2000)
 });
 
-jobQueue.add({name: 'stuff'});
-jobQueue.add({name: 'more'});
-jobQueue.add({name: 'again'});
+dos.process((job, done) => {
+  setTimeout(() => { 
+    console.log('Running...', { jobData: job.data, queue: job.queue.name});
+    done();
+  }, 1500)
+});
 
-// if (jobQueue.start) {
-//   jobQueue.start();
-// }
+dos.on('completed', (job, result) => {
+  console.log('completed', job.data);
+});
+
+uno.on('error', onFail);
+
+function onFail(job, err) {
+  console.log(`JOB [${job.data}] failed with ${err}`);
+}
+
+uno.on('resumed', function(job){
+  console.log('Resuming ', job.data);
+  // The queue has been resumed.
+})
+
+uno.add({name: '1'});
+uno.add({name: '2'});
+uno.add({name: '3'});
+uno.add({name: '4'});
+uno.add({name: '5'});
+uno.add({name: '6'});
+uno.add({name: '7'});
+uno.add({name: '8'});
+
+dos.add({name: 'A'});
+dos.add({name: 'B'});
+dos.add({name: 'C'});
 
 console.log('what up');
